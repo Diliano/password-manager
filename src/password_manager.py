f4047df6ec34fc05e35c8c783dd34c37507f9502
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-11-07 18:48:19 +0000

    build run_password_manager(): menu interface, choice validation, exit choice implemented

---
+++ src/password_manager.py
@@ -0,0 +1,23 @@
+import boto3
+from src.utils import store_secret, list_secrets, retrieve_secret, delete_secret
+
+
+def run_password_manager():
+    secretsmanager_client = boto3.client("secretsmanager")
+
+    while True:
+        choice = input(
+            "\n> Please specify [e]ntry, [r]etrieval, [d]eletion, [l]isting or e[x]it: "
+        )
+
+        if choice not in {"e", "r", "d", "l", "x"}:
+            print("\n⚠️ Invalid input.")
+            continue
+
+        if choice == "x":
+            print("\nThank you. Goodbye")
+            break
+
+
+if __name__ == "__main__":
+    run_password_manager()
