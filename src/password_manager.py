3d84851bde11bbfb1f64f7fd2b8b6a07ecee922b
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-11-08 10:37:01 +0000

    refactor run_password_manager(), combining menu choice validation conditions into one to remove redundancy

--- src/password_manager.py
+++ src/password_manager.py
@@ -12,18 +12,17 @@ def run_password_manager():
     secretsmanager_client = boto3.client("secretsmanager")
 
     while True:
+        # menu interface
         choice = input(
             "\n> Please specify [e]ntry, [r]etrieval, [d]eletion, [l]isting or e[x]it: "
         )
 
-        if not choice:
+        # menu choice validation
+        if not choice or choice not in {"e", "r", "d", "l", "x"}:
             print("\n⚠️ Invalid input.")
             continue
 
-        if choice not in {"e", "r", "d", "l", "x"}:
-            print("\n⚠️ Invalid input.")
-            continue
-
+        # enter secret (store_secret) functionality
         if choice == "e":
             while True:
                 secret_id = input("\n> Secret identifier: ")
@@ -57,6 +56,7 @@ def run_password_manager():
             except:
                 continue
 
+        # exit interface
         if choice == "x":
             print("\nThank you. Goodbye")
             break
